18989de8e116819b0ea50bd1f188147811e21c05
araohatkokate <araohatk@gmail.com>
2024-09-22 02:58:48 -0500

    added padding to sign up page and focused border for text form fields

--- story_line/lib/core/theme/theme.dart
+++ story_line/lib/core/theme/theme.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:story_line/core/theme/app_pallete.dart';
 
 class AppTheme {
-  static final _border = OutlineInputBorder(
-        borderSide: const BorderSide(
-          color: AppPallete.borderColor,
+  static _border([Color color = AppPallete.borderColor]) => OutlineInputBorder(
+        borderSide: BorderSide(
+          color: color,
           width: 3,
         ),
         borderRadius: BorderRadius.circular(10),
@@ -13,8 +13,8 @@ class AppTheme {
     scaffoldBackgroundColor: AppPallete.backgroundColor,
     inputDecorationTheme: InputDecorationTheme(
       contentPadding: const EdgeInsets.all(27),
-      enabledBorder: _border,
-      focusedBorder: _border,
+      enabledBorder: _border(),
+      focusedBorder: _border(AppPallete.gradient2),
     ),
   );
 }

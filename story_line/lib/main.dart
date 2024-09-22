b7ed39f7d99590a7ab6c3cedf917b9bdab7e0501
araohatkokate <araohatk@gmail.com>
2024-09-22 01:23:42 -0500

    setting up color pallete to set theme of app

--- story_line/lib/main.dart
+++ story_line/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:story_line/features/auth/presentation/pages/signup_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,12 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Story Line',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const SignUpPage(),
     );
   }
 }

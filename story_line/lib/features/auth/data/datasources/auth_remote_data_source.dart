5f24ea7ee199948f8981f35170526b7c6cf67408
araohatkokate <araohatk@gmail.com>
2024-09-22 08:09:02 -0500

    implementation of error handling for sign up

---
+++ story_line/lib/features/auth/data/datasources/auth_remote_data_source.dart
@@ -0,0 +1,45 @@
+import 'package:story_line/core/error/exceptions.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+abstract interface class AuthRemoteDataSource {
+   Future<String> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+   });
+
+   Future<String> loginWithEmailPassword({
+    required String email,
+    required String password,
+   });
+}
+
+class AuthRemoteDataSourceImplementation implements AuthRemoteDataSource {
+  final SupabaseClient supabaseClient;
+  AuthRemoteDataSourceImplementation(this.supabaseClient);
+  @override
+  Future<String> loginWithEmailPassword({required String email, required String password}) {
+    // TODO: implement loginWithEmailPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<String> signUpWithEmailPassword({required String name, required String email, required String password}) 
+  async{
+    try {
+      final response = await supabaseClient.auth.signUp(password: password, email: email, data: {'name': name,}
+      );
+
+      if(response.user == null)
+      {
+          throw const ServerException('User is null!');
+      }
+      return response.user!.id;
+    }
+
+    catch (e) {
+      throw ServerException(e.toString());
+    }
+  }
+  
+}

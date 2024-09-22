2de974d06d84ee9d2bb4a2299c2eb21352075149
araohatkokate <araohatk@gmail.com>
2024-09-22 02:50:56 -0500

    creating authentication text form fields

--- story_line/lib/core/theme/theme.dart
+++ story_line/lib/core/theme/theme.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:story_line/core/theme/app_pallete.dart';
 
 class AppTheme {
-  static final darkThemeMode = ThemeData.dark();
+  static final _border = OutlineInputBorder(
+        borderSide: const BorderSide(
+          color: AppPallete.borderColor,
+          width: 3,
+        ),
+        borderRadius: BorderRadius.circular(10),
+      );
+  static final darkThemeMode = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: AppPallete.backgroundColor,
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: const EdgeInsets.all(27),
+      enabledBorder: _border,
+      focusedBorder: _border,
+    ),
+  );
 }

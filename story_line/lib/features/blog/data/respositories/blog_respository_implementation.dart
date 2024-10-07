186c32f47b66ccd5db9a29e1d1f54a3b9c0b572f
araohatkokate <araohatk@gmail.com>
2024-10-07 02:10:14 -0500

    successfully set up local storage to get blogs from hive database

--- story_line/lib/features/blog/data/respositories/blog_respository_implementation.dart
+++ story_line/lib/features/blog/data/respositories/blog_respository_implementation.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 import 'package:fpdart/fpdart.dart';
 import 'package:story_line/core/error/exceptions.dart';
 import 'package:story_line/core/error/failures.dart';
+import 'package:story_line/core/network/connection_checker.dart';
+import 'package:story_line/features/blog/data/datasources/blog_local_data_source.dart';
 import 'package:story_line/features/blog/data/datasources/blog_remote_data_source.dart';
 import 'package:story_line/features/blog/data/models/blog_model.dart';
 import 'package:story_line/features/blog/domain/enitities/blog.dart';
@@ -10,7 +12,9 @@ import 'package:uuid/uuid.dart';
 
 class BlogRespositoryImplementation implements BlogRepository{
   final BlogRemoteDataSource blogRemoteDataSource;
-  BlogRespositoryImplementation(this.blogRemoteDataSource);
+  final BlogLocalDataSource blogLocalDataSource;
+  final ConnectionChecker connectionChecker;
+  BlogRespositoryImplementation(this.blogRemoteDataSource, this.blogLocalDataSource, this.connectionChecker);
 
   @override
   Future<Either<Failure, Blog>> uploadBlog({
@@ -21,6 +25,9 @@ class BlogRespositoryImplementation implements BlogRepository{
     required List<String> topics,
     })  async {
       try{
+        if(!await (connectionChecker.isConnected)) {
+          return left(Failure('No internet connection!'));
+        }
       BlogModel blogModel = BlogModel(
         id: const Uuid().v1(), 
         posterId: posterId, 
@@ -49,7 +56,12 @@ class BlogRespositoryImplementation implements BlogRepository{
   @override
   Future<Either<Failure, List<Blog>>> getAllBlogs() async {
     try {
+      if(!await (connectionChecker.isConnected)) {
+        final blogs = blogLocalDataSource.loadBlogs();
+        return right(blogs);
+      }
       final blogs = await blogRemoteDataSource.getAllBlogs();
+      blogLocalDataSource.uploadLocalBlogs(blogs: blogs);
       return right(blogs);
     }
 
--- story_line/lib/init_dependencies.dart
+++ story_line/lib/init_dependencies.dart
@@ -1,5 +1,7 @@
 import 'package:get_it/get_it.dart';
+import 'package:hive/hive.dart';
 import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:story_line/core/common/cubits/app_user/app_user_cubit.dart';
 import 'package:story_line/core/network/connection_checker.dart';
 import 'package:story_line/core/secrets/app_secrets.dart';
@@ -10,6 +12,7 @@ import 'package:story_line/features/auth/domain/usecases/current_user.dart';
 import 'package:story_line/features/auth/domain/usecases/user_login.dart';
 import 'package:story_line/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:story_line/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:story_line/features/blog/data/datasources/blog_local_data_source.dart';
 import 'package:story_line/features/blog/data/datasources/blog_remote_data_source.dart';
 import 'package:story_line/features/blog/data/respositories/blog_respository_implementation.dart';
 import 'package:story_line/features/blog/domain/repositories/blog_respository.dart';
@@ -24,7 +27,12 @@ Future<void> initDependencies () async {
   _initAuth();
   _initBlog();
   final supabase = await Supabase.initialize(url: AppSecrets.supabaseurl, anonKey: AppSecrets.supabaseanonkey);
+  
+  Hive.defaultDirectory = (await getApplicationDocumentsDirectory()).path;
+
   serviceLocator.registerLazySingleton(() => supabase.client);
+
+  serviceLocator.registerLazySingleton(() => Hive.box(name: 'blogs'),);
 
   serviceLocator.registerFactory(() => InternetConnection());
 
@@ -44,14 +52,22 @@ void _initAuth() {
 
 void _initBlog() {
   serviceLocator
-  ..registerFactory<BlogRemoteDataSource>(
-    () => BlogRemoteDataSourceImplementation(
-      serviceLocator(),
-      ),
-    )
+    ..registerFactory<BlogRemoteDataSource>(
+      () => BlogRemoteDataSourceImplementation(
+        serviceLocator(),
+        ),
+      )
     
+    ..registerFactory<BlogLocalDataSource>(
+      () => BlogLocalDataSourceImplementation(
+        serviceLocator(),
+        ),
+      )
+
     ..registerFactory<BlogRepository>(
       () => BlogRespositoryImplementation(
+        serviceLocator(),
+        serviceLocator(),
         serviceLocator(),
         )
       )

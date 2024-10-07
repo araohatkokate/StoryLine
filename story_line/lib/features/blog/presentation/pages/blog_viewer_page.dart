18d7d66afb7dd757aecb8572d44ace5ebea64fe8
araohatkokate <araohatk@gmail.com>
2024-10-06 19:54:49 -0500

    finished display of individual blog and added back button functionality

--- story_line/lib/features/blog/presentation/pages/blog_viewer_page.dart
+++ story_line/lib/features/blog/presentation/pages/blog_viewer_page.dart
@@ -12,17 +12,26 @@ class BlogViewerPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(blog.title, style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-            const SizedBox(height: 20,),
-            Text('By ${blog.posterName}', style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 16),),
-            const SizedBox(height: 5,),
-            Text('${blog.updatedAt} . ${calculateReadingTime(blog.content)} min', style: const TextStyle(fontWeight: FontWeight.w500, color:  AppPallete.greyColor, fontSize: 16),)
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(blog.title, style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+              const SizedBox(height: 20,),
+              Text('By ${blog.posterName}', style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 16),),
+              const SizedBox(height: 5,),
+              Text('${blog.updatedAt} . ${calculateReadingTime(blog.content)} min', style: const TextStyle(fontWeight: FontWeight.w500, color:  AppPallete.greyColor, fontSize: 16),),
+              const SizedBox(height: 20,),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.network(blog.imageUrl),
+              ),
+              const SizedBox(height: 20,),
+              Text(blog.content, style: const TextStyle(fontSize: 16, height: 2),),
+            ],
+          ),
         ),
       ),
     );
